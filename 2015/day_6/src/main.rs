144bd4b840b38fe9c7c4338009d1f5bdff01f663
Bernatsky Pavel <pasha.desh@yandex.by>
2020-12-05 00:43:11 +0300

    Day 4/6 of 2020/15
    Day 6 of 2015 and day 4 of 2020 were completed.

--- 2015/day_6/src/main.rs
+++ 2015/day_6/src/main.rs
@@ -1,3 +1,135 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
     println!("Hello, world!");
+    part_one();
+    part_two();
+}
+
+fn part_one() {
+    let mut lights = vec![vec![false; 1000]; 1000];
+    let file: File = File::open("input.txt").unwrap();
+    for line in BufReader::new(file).lines() {
+        let string = line.unwrap();
+        let parts = string.split_whitespace().collect::<Vec<&str>>();
+        let new_state;
+        match parts[0] {
+            "turn" => {
+                match parts[1] {
+                    "on" => {
+                        new_state = true;
+                    },
+                    "off" => {
+                        new_state = false;
+                    },
+                    _ => {
+                        unimplemented!();
+                    },
+                }
+                let mut lu_point = (0,0);
+                let mut rb_point = (0,0);
+                let lu_coords = parts[2].split(',').collect::<Vec<&str>>();
+                lu_point.0 = lu_coords[0].parse::<usize>().unwrap();
+                lu_point.1 = lu_coords[1].parse::<usize>().unwrap();
+                let rb_coords = parts[4].split(',').collect::<Vec<&str>>();
+                rb_point.0 = rb_coords[0].parse::<usize>().unwrap();
+                rb_point.1 = rb_coords[1].parse::<usize>().unwrap();
+                (lu_point.0..=rb_point.0).for_each(|x|{
+                    (lu_point.1..=rb_point.1).for_each(|y|{
+                        lights[x][y] = new_state;
+                    });
+                });
+            },
+            "toggle" => {
+                let mut lu_point = (0,0);
+                let mut rb_point = (0,0);
+                let lu_coords = parts[1].split(',').collect::<Vec<&str>>();
+                lu_point.0 = lu_coords[0].parse::<usize>().unwrap();
+                lu_point.1 = lu_coords[1].parse::<usize>().unwrap();
+                let rb_coords = parts[3].split(',').collect::<Vec<&str>>();
+                rb_point.0 = rb_coords[0].parse::<usize>().unwrap();
+                rb_point.1 = rb_coords[1].parse::<usize>().unwrap();
+                (lu_point.0..=rb_point.0).for_each(|x|{
+                    (lu_point.1..=rb_point.1).for_each(|y|{
+                        lights[x][y] = !lights[x][y];
+                    });
+                });
+            },
+            _ => {},
+        };
+    }
+    let mut lit_counter = 0;
+    lights.iter().for_each(|row|{
+        row.iter().for_each(|&light|{
+            if light {
+                lit_counter += 1;
+            }
+        })
+    });
+    println!("{}", lit_counter);
+}
+
+fn part_two() {
+    let mut lights = vec![vec![0_usize; 1000]; 1000];
+    let file: File = File::open("input.txt").unwrap();
+    for line in BufReader::new(file).lines() {
+        let string = line.unwrap();
+        let parts = string.split_whitespace().collect::<Vec<&str>>();
+        let new_state;
+        match parts[0] {
+            "turn" => {
+                match parts[1] {
+                    "on" => {
+                        new_state = true;
+                    },
+                    "off" => {
+                        new_state = false;
+                    },
+                    _ => {
+                        unimplemented!();
+                    },
+                }
+                let mut lu_point = (0,0);
+                let mut rb_point = (0,0);
+                let lu_coords = parts[2].split(',').collect::<Vec<&str>>();
+                lu_point.0 = lu_coords[0].parse::<usize>().unwrap();
+                lu_point.1 = lu_coords[1].parse::<usize>().unwrap();
+                let rb_coords = parts[4].split(',').collect::<Vec<&str>>();
+                rb_point.0 = rb_coords[0].parse::<usize>().unwrap();
+                rb_point.1 = rb_coords[1].parse::<usize>().unwrap();
+                (lu_point.0..=rb_point.0).for_each(|x|{
+                    (lu_point.1..=rb_point.1).for_each(|y|{
+                        if new_state {
+                            lights[x][y] = lights[x][y].saturating_add(1);
+                        } else {
+                            lights[x][y] = lights[x][y].saturating_sub(1);
+                        }
+                        
+                    });
+                });
+            },
+            "toggle" => {
+                let mut lu_point = (0,0);
+                let mut rb_point = (0,0);
+                let lu_coords = parts[1].split(',').collect::<Vec<&str>>();
+                lu_point.0 = lu_coords[0].parse::<usize>().unwrap();
+                lu_point.1 = lu_coords[1].parse::<usize>().unwrap();
+                let rb_coords = parts[3].split(',').collect::<Vec<&str>>();
+                rb_point.0 = rb_coords[0].parse::<usize>().unwrap();
+                rb_point.1 = rb_coords[1].parse::<usize>().unwrap();
+                (lu_point.0..=rb_point.0).for_each(|x|{
+                    (lu_point.1..=rb_point.1).for_each(|y|{
+                        lights[x][y] = lights[x][y].saturating_add(2);
+                    });
+                });
+            },
+            _ => {},
+        };
+    }
+    let lit_counter =
+    lights.iter().map(|row|{
+        row.iter().sum::<usize>()
+    }).sum::<usize>();
+    println!("{}", lit_counter);
 }
--- 2020/day_4/src/main.rs
+++ 2020/day_4/src/main.rs
@@ -1,41 +1,154 @@
+use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::BTreeMap;
 
 fn main() {
     println!("Hello, world!");
-    println!("{}", part_one());
+    let mut valid_passes = part_one();
+    println!("{}", valid_passes.len());
+    part_two(&mut valid_passes);
+    println!("{}", valid_passes.len());
 }
 
-fn part_one() -> usize {
+fn part_one() -> Vec<BTreeMap<String, String>> {
     let file: File = File::open("input.txt").unwrap();
-    let mut valid_counter = 0;
-    let required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-    let all_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"];
-    let mut pass: BTreeMap<&str, &str> = BTreeMap::new();
+    let required_fields = [
+        String::from("byr"),
+        String::from("iyr"),
+        String::from("eyr"),
+        String::from("hgt"),
+        String::from("hcl"),
+        String::from("ecl"),
+        String::from("pid"),
+    ];
+    let all_fields = [
+        String::from("byr"),
+        String::from("iyr"),
+        String::from("eyr"),
+        String::from("hgt"),
+        String::from("hcl"),
+        String::from("ecl"),
+        String::from("pid"),
+        String::from("cid"),
+    ];
+    let mut passports: BTreeMap<String, String> = BTreeMap::new();
+    let mut valid_passports: Vec<BTreeMap<String, String>> = Vec::new();
     for line in BufReader::new(file).lines() {
         let string = line.unwrap();
         if string.is_empty() {
-            let keys = pass.keys().collect::<Vec<&&str>>();
-            let is_all_presented = all_fields.iter().all(|key| {
-                keys.contains(&key)
-            });
+            let keys = passports.keys().collect::<Vec<&String>>();
+            let is_all_presented = all_fields.iter().all(|key| keys.contains(&key));
             if is_all_presented {
-                valid_counter += 1;
+                valid_passports.push(passports.clone());
             } else {
-                if required_fields.iter().all(|key|{
-                    keys.contains(&key)
-                }) {
-                    valid_counter +=1;
+                if required_fields.iter().all(|key| keys.contains(&key)) {
+                    valid_passports.push(passports.clone());
                 }
             }
-            pass.clear();
+            passports.clear();
         } else {
             string.split_whitespace().for_each(|fields| {
                 let values = fields.split(":").collect::<Vec<&str>>();
-                pass.insert(values[0].clone(), values[1]);
+                passports.insert(String::from(values[0]), String::from(values[1]));
             });
         }
     }
-    valid_counter
+    valid_passports
+}
+
+fn part_two(passports: &mut Vec<BTreeMap<String, String>>) {
+    passports.retain(|passport| {
+        let mut is_valid = true;
+        passport.iter().for_each(|fields| match fields.0.as_str() {
+            "byr" => {
+                let temp = fields.1.parse::<usize>();
+                if temp.is_err() {
+                    is_valid = false;
+                } else {
+                    let byr = temp.unwrap();
+                    if byr < 1920 || byr > 2002 {
+                        is_valid = false;
+                    }
+                }
+            }
+            "iyr" => {
+                let temp = fields.1.parse::<usize>();
+                if temp.is_err() {
+                    is_valid = false;
+                } else {
+                    let iyr = temp.unwrap();
+                    if iyr < 2010 || iyr > 2020 {
+                        is_valid = false;
+                    }
+                }
+            }
+            "eyr" => {
+                let temp = fields.1.parse::<usize>();
+                if temp.is_err() {
+                    is_valid = false;
+                } else {
+                    let eyr = temp.unwrap();
+                    if eyr < 2020 || eyr > 2030 {
+                        is_valid = false;
+                    }
+                }
+            }
+            "hgt" => {
+                let mut measurments = fields.1.clone();
+                measurments.retain(|ch| !ch.is_numeric());
+                let mut height = fields.1.clone();
+                height.retain(|ch| ch.is_numeric());
+                let height = height.parse::<usize>();
+                if height.is_err() {
+                    is_valid = false;
+                } else {
+                    let height = height.unwrap();
+                    match measurments.as_str() {
+                        "cm" => {
+                            if height < 150 || height > 193 {
+                                is_valid = false;
+                            }
+                        }
+                        "in" => {
+                            if height < 59 || height > 76 {
+                                is_valid = false;
+                            }
+                        }
+                        _ => {
+                            is_valid = false;
+                        }
+                    }
+                }
+            }
+            "hcl" => {
+                let hcl = fields.1.clone();
+                let hex_color = hcl.strip_prefix("#");
+                if hex_color.is_none() {
+                    is_valid = false;
+                } else {
+                    let hex_color = hex_color.unwrap();
+                    if hex_color.len() != 6 || !hex_color.chars().all(|ch| ch.is_ascii_hexdigit()) {
+                        is_valid = false;
+                    }
+                }
+            }
+            "ecl" => {
+                if !["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&fields.1.as_str()) {
+                    is_valid = false;
+                }
+            }
+            "pid" => {
+                let pid = fields.1.parse::<usize>();
+                if pid.is_err() {
+                    is_valid = false;
+                } else {
+                    if fields.1.len() != 9 {
+                        is_valid = false;
+                    }
+                }
+            }
+            _ => {}
+        });
+        is_valid
+    });
 }
